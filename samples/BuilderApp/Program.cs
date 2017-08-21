f08794354494823bb7a1dd142707cf9757bcd6b3
Andy Gerlicher <angerlic@microsoft.com>
2017-08-21 10:50:24 -0700

    Initial commit. Adding basic functionality / prototype.

---
+++ samples/BuilderApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using Microsoft.Build.Construction;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Framework;
+
+namespace BuilderApp
+{
+    internal class Program
+    {
+        private static readonly string s_msBuildPath = MSBuildLocator.MSBuildLocator.LoadDefaults();
+
+        private static void Main(string[] args)
+        {
+            if (args.Length < 1 || !File.Exists(args[0])) Usage();
+
+            var projectFilePath = args[0];
+            Console.WriteLine($"Building {projectFilePath} with MSBuild from {s_msBuildPath}.");
+
+            var pre = ProjectRootElement.Open(projectFilePath);
+            var project = new Project(pre);
+            var result = project.Build(new Logger());
+            Console.WriteLine($"Build result: {result}");
+        }
+
+        private static void Usage()
+        {
+            Console.WriteLine("BuilderApp.exe <path>");
+            Console.WriteLine("    path = path to .*proj file to build");
+            Environment.Exit(-1);
+        }
+
+        private class Logger : ILogger
+        {
+            public void Initialize(IEventSource eventSource)
+            {
+                eventSource.AnyEventRaised += (sender, args) => { Console.WriteLine(args.Message); };
+            }
+
+            public void Shutdown()
+            {
+            }
+
+            public LoggerVerbosity Verbosity { get; set; }
+            public string Parameters { get; set; }
+        }
+    }
+}
---
+++ src/MSBuildLocator/MSBuildLocator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Build.Shared;
+
+namespace MSBuildLocator
+{
+    public static class MSBuildLocator
+    {
+        private static readonly string[] s_msBuildAssemblies = {"Microsoft.Build", "Microsoft.Build.Framework"};
+        private static readonly Lazy<string> s_msbuildPath = new Lazy<string>(GetMSBuildPath);
+
+        public static string LoadDefaults()
+        {
+            if (s_msbuildPath.Value != null)
+            {
+                AppDomain.CurrentDomain.AssemblyResolve += (sender, eventArgs) =>
+                {
+                    var assemblyName = new AssemblyName(eventArgs.Name);
+                    if (s_msBuildAssemblies.Contains(assemblyName.Name, StringComparer.OrdinalIgnoreCase))
+                    {
+                        var targetAssembly = Path.Combine(s_msbuildPath.Value, assemblyName.Name + ".dll");
+                        return File.Exists(targetAssembly) ? Assembly.LoadFrom(targetAssembly) : null;
+                    }
+
+                    return null;
+                };
+
+                return s_msbuildPath.Value;
+            }
+
+            return null;
+        }
+
+        private static string GetMSBuildPath()
+        {
+            // Dev console, probably the best case
+            var vsinstalldir = Environment.GetEnvironmentVariable("VSINSTALLDIR");
+            if (!string.IsNullOrEmpty(vsinstalldir))
+                return FindMSBuildFromVisualStudioPath(vsinstalldir);
+
+            // Check for VS instances
+            var instances = VisualStudioLocationHelper.GetInstances();
+
+            if (instances.Count == 0)
+                throw new Exception("Couldn't find MSBuild");
+
+
+            return FindMSBuildFromVisualStudioPath(instances.First().Path);
+        }
+
+        private static string FindMSBuildFromVisualStudioPath(string visualStudioPath)
+        {
+            if (string.IsNullOrEmpty(visualStudioPath)) return null;
+
+            var path = Path.Combine(visualStudioPath, "MSBuild", "15.0", "Bin");
+            return path;
+
+            // TODO: Check if it exists?
+            //var msBuildExe = Path.Combine(path, "MSBuild.exe");
+            //var msBuildExeConfig = Path.Combine(path, "MSBuild.exe");
+            //if (File.Exists(msb) && File.Exists(msBuildExeConfig)) {  ... }
+        }
+    }
+}
