ab5e60247ef8d8ac255377c0be039c34498d84a0
Andy Gerlicher <angerlic@microsoft.com>
2017-08-25 14:59:20 -0700

    Initial commit. (#1)
    * Adding first version of API. Still subject to change.
    * Added sample app to show basic usage.

---
+++ samples/BuilderApp/Program.cs
@@ -0,0 +1,123 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Construction;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Framework;
+using Microsoft.Build.MSBuildLocator;
+
+namespace BuilderApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Header();
+            var projectFilePath = Args(args);
+
+            // Before we can build we need to resolve MSBuild assemblies. We could:
+            //   1) Use defaults and call: MSBuildLocator.RegisterDefaults();
+            //   2) Do something fancier and ask the user. As an example we'll do that.
+            var instances = MSBuildLocator.QueryVisualStudioInstances().ToList();
+            var instanceToUse = AskWhichVisualStudioInstanceToUse(instances);
+
+            // Calling RegisterInstance will subscribe to AssemblyResolve event. After this we can now
+            // safely call code that use MSBuild types (in the Builder class).
+            MSBuildLocator.RegisterInstance(instanceToUse);
+
+            Console.WriteLine($"Using VS Instance: {instanceToUse.Name} - {instanceToUse.Version}");
+            Console.WriteLine();
+
+            var result = new Builder().Build(projectFilePath);
+            Console.WriteLine($"Build result: {result}");
+        }
+
+        private static VisualStudioInstance AskWhichVisualStudioInstanceToUse(List<VisualStudioInstance> instances)
+        {
+            if (instances.Count == 0)
+            {
+                Console.WriteLine("MSBuild not found! Exiting.");
+                Environment.Exit(-1);
+            }
+
+            for (var i = 1; i <= instances.Count; i++)
+            {
+                var instance = instances[i - 1];
+                var recommended = string.Empty;
+
+                // The dev console is probably always the right choice because the user explicitly opened
+                // one associated with a Visual Studio install. It will always be first in the list.
+                if (instance.DiscoveryType == DiscoveryType.DeveloperConsole)
+                    recommended = " (Recommended!)";
+
+                Console.WriteLine($"{i}) {instance.Name} - {instance.Version}{recommended}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Select an instance of MSBuild: ");
+            var answer = Console.ReadLine();
+            VisualStudioInstance instanceUsed = null;
+
+            if (int.TryParse(answer, out int instanceChoice) && instanceChoice > 0 && instanceChoice <= instances.Count)
+            {
+                instanceUsed = instances[instanceChoice - 1];
+            }
+            else
+            {
+                Console.WriteLine($"{answer} is not a valid response.");
+                Environment.Exit(-1);
+            }
+
+            return instanceUsed;
+        }
+
+        private static void Header()
+        {
+            Console.WriteLine($"Sample MSBuild Builder App {ThisAssembly.AssemblyInformationalVersion}.");
+            Console.WriteLine();
+        }
+
+        private static string Args(string[] args)
+        {
+            if (args.Length < 1 || !File.Exists(args[0])) Usage();
+            var projectFilePath = args[0];
+            return projectFilePath;
+        }
+
+        private static void Usage()
+        {
+            Console.WriteLine("BuilderApp.exe <path>");
+            Console.WriteLine("    path = path to .*proj file to build");
+            Environment.Exit(-1);
+        }
+    }
+
+    public class Builder
+    {
+        public bool Build(string projectFile)
+        {
+            var pre = ProjectRootElement.Open(projectFile);
+            var project = new Project(pre);
+            return project.Build(new Logger());
+        }
+
+        private class Logger : ILogger
+        {
+            public void Initialize(IEventSource eventSource)
+            {
+                eventSource.AnyEventRaised += (_, args) => { Console.WriteLine(args.Message); };
+            }
+
+            public void Shutdown()
+            {
+            }
+
+            public LoggerVerbosity Verbosity { get; set; }
+            public string Parameters { get; set; }
+        }
+    }
+}
---
+++ src/MSBuildLocator/MSBuildLocator.cs
@@ -0,0 +1,113 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Microsoft.Build.MSBuildLocator
+{
+    public static class MSBuildLocator
+    {
+        private static readonly string[] s_msBuildAssemblies =
+        {
+            "Microsoft.Build", "Microsoft.Build.Framework", "Microsoft.Build.Tasks.Core",
+            "Microsoft.Build.Utilities.Core"
+        };
+
+        /// <summary>
+        ///     Query for all Visual Studio instances.
+        /// </summary>
+        /// <remarks>
+        ///     Only includes Visual Studio 2017 (v15.0) and higher.
+        /// </remarks>
+        /// <returns>Enumeration of all Visual Studio instances detected on the machine.</returns>
+        public static IEnumerable<VisualStudioInstance> QueryVisualStudioInstances()
+        {
+            return QueryVisualStudioInstances(VisualStudioInstanceQueryOptions.Default);
+        }
+
+        /// <summary>
+        ///     Query for Visual Studio instances matching the given options.
+        /// </summary>
+        /// <remarks>
+        ///     Only includes Visual Studio 2017 (v15.0) and higher.
+        /// </remarks>
+        /// <param name="options">Query options for Visual Studio instances.</param>
+        /// <returns>Enumeration of Visual Studio instances detected on the machine.</returns>
+        public static IEnumerable<VisualStudioInstance> QueryVisualStudioInstances(
+            VisualStudioInstanceQueryOptions options)
+        {
+            return GetInstances().Where(i => i.DiscoveryType.HasFlag(options.DiscoveryTypes));
+        }
+
+        /// <summary>
+        ///     Discover instances of Visual Studio and register the first one. See <see cref="RegisterInstance" />.
+        /// </summary>
+        /// <returns>Instance of Visual Studio found and registered.</returns>
+        public static VisualStudioInstance RegisterDefaults()
+        {
+            var instance = GetInstances().FirstOrDefault();
+            RegisterInstance(instance);
+
+            return instance;
+        }
+
+        /// <summary>
+        ///     Add assembly resolution for Microsoft.Build core dlls in the current AppDomain from the specified
+        ///     instance of Visual Studio. See <see cref="QueryVisualStudioInstances()" /> to discover Visual Studio
+        ///     instances or use <see cref="RegisterDefaults" />.
+        /// </summary>
+        /// <param name="instance"></param>
+        public static void RegisterInstance(VisualStudioInstance instance)
+        {
+            if (instance == null)
+                throw new ArgumentNullException(nameof(instance));
+
+            AppDomain.CurrentDomain.AssemblyResolve += (_, eventArgs) =>
+            {
+                var assemblyName = new AssemblyName(eventArgs.Name);
+                if (s_msBuildAssemblies.Contains(assemblyName.Name, StringComparer.OrdinalIgnoreCase))
+                {
+                    var targetAssembly = Path.Combine(instance.MSBuildPath, assemblyName.Name + ".dll");
+                    return File.Exists(targetAssembly) ? Assembly.LoadFrom(targetAssembly) : null;
+                }
+
+                return null;
+            };
+        }
+
+        private static IEnumerable<VisualStudioInstance> GetInstances()
+        {
+            var devConsole = GetDevConsoleInstance();
+            if (devConsole != null)
+                yield return devConsole;
+
+            foreach (var instance in VisualStudioLocationHelper.GetInstances())
+                yield return instance;
+        }
+
+        private static VisualStudioInstance GetDevConsoleInstance()
+        {
+            var path = Environment.GetEnvironmentVariable("VSINSTALLDIR");
+            if (!string.IsNullOrEmpty(path))
+            {
+                var versionString = Environment.GetEnvironmentVariable("VSCMD_VER");
+                Version version;
+                Version.TryParse(versionString, out version);
+
+                if (version == null)
+                {
+                    versionString = Environment.GetEnvironmentVariable("VisualStudioVersion");
+                    Version.TryParse(versionString, out version);
+                }
+
+                return new VisualStudioInstance("DEVCONSOLE", path, version, DiscoveryType.DeveloperConsole);
+            }
+
+            return null;
+        }
+    }
+}
