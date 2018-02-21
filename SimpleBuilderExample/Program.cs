95d29bb9ac328cd35193563f033d82a5e615bb57
Rainer Sigwald <raines@microsoft.com>
2018-02-21 13:50:03 -0600

    Simple example of MSBuild 14 API use

---
+++ SimpleBuilderExample/Program.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Build.Execution;
+using Microsoft.Build.Framework;
+using System.Collections.Generic;
+
+namespace SimpleBuilderExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new BuildParameters
+            {
+                MaxNodeCount = 4,
+                Loggers = new ILogger[] { new Microsoft.Build.Logging.ConsoleLogger(LoggerVerbosity.Normal) },
+            };
+
+            var req = new BuildRequestData("ProjectToBeBuilt.proj",
+                new Dictionary<string, string>(), 
+                null, 
+                new[] { "Build" }, 
+                null, 
+                BuildRequestDataFlags.None);
+
+            var result = BuildManager.DefaultBuildManager.Build(p, req);
+        }
+    }
+}
