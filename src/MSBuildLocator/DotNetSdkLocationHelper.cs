3664dd6490797f9ea70234674e2fcd5062758c7f
Joey Robichaud <jorobich@microsoft.com>
2018-10-19 11:57:32 -0700

    Target netcoreapp2.0 instead of netstandard2.0

--- src/MSBuildLocator/DotNetSdkLocationHelper.cs
+++ src/MSBuildLocator/DotNetSdkLocationHelper.cs
@@ -11,7 +11,7 @@ namespace Microsoft.Build.Locator
 {
     internal static class DotNetSdkLocationHelper
     {
-        private static readonly Regex DotNetBasePathRegex = new Regex("Base Path:(.*)$", RegexOptions.Compiled | RegexOptions.Multiline);
+        private static readonly Regex DotNetBasePathRegex = new Regex("Base Path:(.*)$", RegexOptions.Multiline);
 
         public static VisualStudioInstance GetInstance(string workingDirectory)
         {
