3108a59e0a97077e7d4aa97e6a9fe918405c20f2
Ankit Jain <radical@gmail.com>
2021-01-05 14:54:34 -0500

    remove unused usings

--- src/MSBuildLocator/MSBuildLocator.cs
+++ src/MSBuildLocator/MSBuildLocator.cs
@@ -3,13 +3,10 @@
 
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading;
 
 #if NETCOREAPP
