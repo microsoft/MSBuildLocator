e02e0edc1dc6516fb39afa6f01525b9375c381fe
Ankit Jain <radical@gmail.com>
2021-01-05 23:16:19 -0500

    address review feedback

--- src/MSBuildLocator/MSBuildLocator.Mono.cs
+++ src/MSBuildLocator/MSBuildLocator.Mono.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -45,7 +48,7 @@ namespace Microsoft.Build.Locator
             {
                 ver = null;
                 if (!File.Exists(Path.Combine(path, s_monoMSBuildDll_Current_RelativePath)) &&
-                        !File.Exists(Path.Combine(path, s_monoMSBuildDll_15_0_RelativePath)))
+                    !File.Exists(Path.Combine(path, s_monoMSBuildDll_15_0_RelativePath)))
                 {
                     return false;
                 }
