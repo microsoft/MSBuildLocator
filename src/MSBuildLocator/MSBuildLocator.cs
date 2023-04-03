388601c1fe6020af89125e51aac3651e1a898387
Forgind <Forgind@users.noreply.github.com>
2023-04-03 11:22:53 -0700

    Sort usings

--- src/MSBuildLocator/MSBuildLocator.cs
+++ src/MSBuildLocator/MSBuildLocator.cs
@@ -3,12 +3,12 @@
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using System.ComponentModel;
 
 #if NETCOREAPP
 using System.Runtime.Loader;
