1a944c260918a8d7f6354a8d5a0da5118ce3a04d
YuliiaKovalova <ykovalova@microsoft.com>
2023-12-14 16:26:49 +0100

    Remove the max version limitation & add package tags

--- src/MSBuildLocator/DotNetSdkLocationHelper.cs
+++ src/MSBuildLocator/DotNetSdkLocationHelper.cs
@@ -52,16 +52,6 @@ namespace Microsoft.Build.Locator
                 return null;
             }
 
-            // Components of the SDK often have dependencies on the runtime they shipped with, including that several tasks that shipped
-            // in the .NET 5 SDK rely on the .NET 5.0 runtime. Assuming the runtime that shipped with a particular SDK has the same version,
-            // this ensures that we don't choose an SDK that doesn't work with the runtime of the chosen application. This is not guaranteed
-            // to always work but should work for now.
-            if (major > Environment.Version.Major ||
-                (major == Environment.Version.Major && minor > Environment.Version.Minor))
-            {
-                return null;
-            }
-
             return new VisualStudioInstance(
                 name: ".NET Core SDK",
                 path: dotNetSdkPath,
